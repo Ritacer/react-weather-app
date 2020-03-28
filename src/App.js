361ebea9d2594fd24168921cd538409e7cc73d15
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-03-28 16:26:30 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a href="https://github.com/Ritacer/react-weather-app" target="_blank">
-          Open sourced
-        </a>{" "}
-        code by{" "}
-        <a
-          href="https://www.linkedin.com/in/rita-cerqueira-20198766"
-          target="_blank"
-        >
-          Rita Cerqueira
-        </a>
-        , 2020.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/Ritacer/react-weather-app"
+            target="_blank"
+          >
+            Open-source
+          </a>{" "}
+          code by{" "}
+          <a
+            href="https://www.linkedin.com/in/rita-cerqueira-20198766"
+            target="_blank"
+          >
+            Rita Cerqueira
+          </a>
+          , 2020.
+        </footer>
+      </div>
     </div>
   );
 }
