57faaf34bd1f41861ca837198fcee33ab1c6696f
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-03-28 12:46:03 +0000

    Added GitHub Repo Url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a href="https://github.com/Ritacer/react-weather-app" target="_blank">
+          Open sourced
+        </a>{" "}
+        code by{" "}
+        <a
+          href="https://www.linkedin.com/in/rita-cerqueira-20198766"
+          target="_blank"
+        >
+          Rita Cerqueira
+        </a>
+        , 2020.
+      </footer>
+    </div>
+  );
 }
