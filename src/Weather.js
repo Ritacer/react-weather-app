b403407116f1210505e0761c228bbf1c4b60ed6c
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-03-31 15:52:19 +0100

    Styled the css of the project

--- src/Weather.js
+++ src/Weather.js
@@ -6,54 +6,53 @@ export default function Weather() {
     <div className="Weather">
       <div className="row">
         <div className="col-5">
-          <div className="row">
-            <div className="col-auto">
-              <form className="form-inline">
-                <label className="sr-only" for="inlineFormInputName2">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control mb-2 mr-sm-2"
-                  placeholder="Enter city"
-                  autocomplete="off"
-                />
-                <div>
-                  <button type="submit" className="btn btn-primary mb-2">
-                    Submit
-                  </button>
-                </div>
-              </form>
-              <form>
-                <button
-                  type="button"
-                  class="btn btn-primary mb-2"
-                  id="selector-button"
-                >
-                  Current city
-                </button>
-              </form>
-              <div className="overview">
-                <div className="newOverview">
-                  <h1>Lisbon</h1>
-                  <ul>
-                    <li>Last updated: Saturday, 17:33</li>
-                    <li>Description: Sunny</li>
-                  </ul>
-                </div>
-                <div className="clearfix">
-                  <img
-                    src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                    alt="sunny"
-                  />
-                  <strong>17</strong>ºC
-                </div>
-                <ul>
-                  <li>Humidity: 5%</li>
-                  <li>Wind: 3km/h</li>
-                </ul>
-              </div>
+          <form className="form-inline">
+            <label className="sr-only" for="inlineFormInputName2">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control mb-2 mr-sm-2 w-50"
+              placeholder="Enter city"
+              autoComplete="off"
+              autoFocus="on"
+            />
+            <div>
+              <button type="submit" className="btn btn-primary mb-2">
+                Submit
+              </button>
             </div>
+          </form>
+          <form>
+            <button
+              type="button"
+              class="btn btn-primary mb-2"
+              id="selector-button"
+            >
+              Current city
+            </button>
+          </form>
+          <div className="overview">
+            <div className="newOverview">
+              <h1>Lisbon</h1>
+              <ul>
+                <li>Last updated: Saturday, 17:33</li>
+                <li>Description: Sunny</li>
+              </ul>
+            </div>
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                alt="sunny"
+                className="float-left"
+              />
+              <strong>17</strong>
+              <span className="units">ºC</span>
+            </div>
+            <ul>
+              <li>Humidity: 5%</li>
+              <li>Wind: 3km/h</li>
+            </ul>
           </div>
         </div>
         <div className="col-7">
